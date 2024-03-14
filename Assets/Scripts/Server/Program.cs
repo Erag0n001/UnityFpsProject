3eee5b2ddfb1b4a9f29dd12b933672318d6756bf
Erag0n001 <148992045+Erag0n001@users.noreply.github.com>
2024-03-14 18:01:46 -0400

    Added server

---
+++ Assets/Scripts/Server/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+
+namespace Test
+{
+    public class Test 
+    {
+        public static IPAddress iPAddress = IPAddress.Parse("0.0.0.0");
+        public static int port = int.Parse("25555");
+
+        public static TcpListener server;
+        public static void Main()
+        {
+            server = new TcpListener(iPAddress, port);
+            server.Start();
+            
+            while(true) {ListenForConnections(); }
+        }
+        public static void ListenForConnections()
+        {
+            TcpClient newTCP = server.AcceptTcpClient();
+            Console.WriteLine(newTCP.ToString());
+            Socializing socializing = new Socializing(newTCP);
+        }
+
+
+
+}
+}
---
+++ Assets/Scripts/Server/Socializing.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.Sockets;
+
+public class Socializing
+{
+    public TcpClient connection;
+    public NetworkStream networkStream;
+
+    public StreamWriter streamWriter;
+    public StreamReader streamReader;
+
+    public bool shouldClose;
+    public Socializing(TcpClient connection)
+    {
+        this.connection = connection;
+        networkStream = connection.GetStream();
+        streamWriter = new StreamWriter(networkStream);
+        streamReader = new StreamReader(networkStream);
+        Task.Run(delegate { Listen(); });
+        Task.Run(delegate { Send(); });
+        Task.Run(delegate { CheckConnection(); });
+    }
+    public void Listen()
+    {
+        try
+        {
+            while (true)
+            {
+                Thread.Sleep(1);
+                string data = streamReader.ReadLine();
+                if (string.IsNullOrEmpty(data)) { continue; }
+                else
+                {
+                    Console.WriteLine(data);
+                    shouldClose = false;
+                }
+            }
+        } catch(Exception ex) { Console.WriteLine(ex); }
+        KillConnection();
+    }
+    public void Send()
+    {
+        try
+        {
+            while (true)
+            {
+                Thread.Sleep(1);
+                string data = "test";
+                streamWriter.WriteLine(data);
+                streamWriter.Flush();
+            }
+        } catch (Exception ex) { Console.WriteLine(ex); }
+        KillConnection();
+    }
+
+    public void CheckConnection()
+    {
+        try
+        {
+            while (true)
+            {
+                Thread.Sleep(15000);
+                if (shouldClose)
+                {
+                    break;
+                }
+                else
+                {
+                    shouldClose = true;
+                }
+            }
+        } catch(Exception ex) { Console.WriteLine(ex); }
+        KillConnection();
+    }
+
+    public void KillConnection()
+    {
+        connection.Close();
+        connection.Dispose();
+    }
+}
